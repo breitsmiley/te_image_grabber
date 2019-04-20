13d9c849c62c191bc03888430d0ae95a780c26c6
Pavel Tsarenko <breitsmiley@gmail.com>
2019-04-20 13:47:53 +0300

    [docker] added ImageMagick to php container

--- docker/base-images/te-php-fpm-dev/entrypoint-app.sh
+++ docker/base-images/te-php-fpm-dev/entrypoint-app.sh
@@ -2,10 +2,12 @@
 set -e
 
 if [ "$APP_D_FOR" == 'sf'  ]; then
-    mkdir -p var/cache var/logs var/sessions
-    composer install
-    php bin/console cache:clear
+#    mkdir -p var/cache var/logs var/sessions public/image_store
+#    composer install
+#    php bin/console cache:clear
 #    chown -R www-data var
+    mkdir -p public/image_storage
+    chown -R www-data:www-data public/image_storage
 
 #    echo "Waiting for MySQL to be ready..."
 #    until php bin/console doctrine:migrations:migrate; do
